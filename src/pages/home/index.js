be557117867de34a159839c642f86ddd1d2a896e
AvtarDabhiInventam <108725375+AvtarDabhiInventam@users.noreply.github.com>
2023-05-29 18:34:27 +0530

    add material ui table

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,35 +4,498 @@ import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import CardHeader from '@mui/material/CardHeader'
 import CardContent from '@mui/material/CardContent'
+import MaterialReactTable from 'material-react-table'
+import { useCallback, useMemo, useState } from 'react'
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Divider,
+  FormControl,
+  IconButton,
+  InputLabel,
+  MenuItem,
+  Select,
+  Stack,
+  TextField,
+  Tooltip
+} from '@mui/material'
+import { useDispatch } from 'react-redux'
+import { Delete, Edit } from '@mui/icons-material'
+import HorizontalLayout from 'src/@core/layouts/HorizontalLayout'
+
+// import { data, states } from './makeData'
+
+//50 us states array
+export const states = [
+  'Alabama',
+  'Alaska',
+  'Arizona',
+  'Arkansas',
+  'California',
+  'Colorado',
+  'Connecticut',
+  'Delaware',
+  'Florida',
+  'Georgia',
+  'Hawaii',
+  'Idaho',
+  'Illinois',
+  'Indiana',
+  'Iowa',
+  'Kansas',
+  'Kentucky',
+  'Louisiana',
+  'Maine',
+  'Maryland',
+  'Massachusetts',
+  'Michigan',
+  'Minnesota',
+  'Mississippi',
+  'Missouri',
+  'Montana',
+  'Nebraska',
+  'Nevada',
+  'New Hampshire',
+  'New Jersey',
+  'New Mexico',
+  'New York',
+  'North Carolina',
+  'North Dakota',
+  'Ohio',
+  'Oklahoma',
+  'Oregon',
+  'Pennsylvania',
+  'Rhode Island',
+  'South Carolina',
+  'South Dakota',
+  'Tennessee',
+  'Texas',
+  'Utah',
+  'Vermont',
+  'Virginia',
+  'Washington',
+  'West Virginia',
+  'Wisconsin',
+  'Wyoming',
+  'Puerto Rico'
+]
+
+const data = [
+  {
+    id: '9s41rp',
+    firstName: 'Kelvin',
+    lastName: 'Langosh',
+    email: 'Jerod14@hotmail.com',
+    age: 19,
+    state: 'Ohio'
+  },
+  {
+    id: '08m6rx',
+    firstName: 'Molly',
+    lastName: 'Purdy',
+    email: 'Hugh.Dach79@hotmail.com',
+    age: 37,
+    state: 'Rhode Island'
+  },
+  {
+    id: '5ymtrc',
+    firstName: 'Henry',
+    lastName: 'Lynch',
+    email: 'Camden.Macejkovic@yahoo.com',
+    age: 20,
+    state: 'California'
+  },
+  {
+    id: 'ek5b97',
+    firstName: 'Glenda',
+    lastName: 'Douglas',
+    email: 'Eric0@yahoo.com',
+    age: 38,
+    state: 'Montana'
+  },
+  {
+    id: 'xxtydd',
+    firstName: 'Leone',
+    lastName: 'Williamson',
+    email: 'Ericka_Mueller52@yahoo.com',
+    age: 19,
+    state: 'Colorado'
+  },
+  {
+    id: 'wzxj9m',
+    firstName: 'Mckenna',
+    lastName: 'Friesen',
+    email: 'Veda_Feeney@yahoo.com',
+    age: 34,
+    state: 'New York'
+  },
+  {
+    id: '21dwtz',
+    firstName: 'Wyman',
+    lastName: 'Jast',
+    email: 'Melvin.Pacocha@yahoo.com',
+    age: 23,
+    state: 'Montana'
+  },
+  {
+    id: 'o8oe4k',
+    firstName: 'Janick',
+    lastName: 'Willms',
+    email: 'Delfina12@gmail.com',
+    age: 25,
+    state: 'Nebraska'
+  }
+]
 
 const Home = () => {
+  const [createModalOpen, setCreateModalOpen] = useState(false)
+  const [tableData, setTableData] = useState(() => data)
+  const [validationErrors, setValidationErrors] = useState({})
+
+  const handleCreateNewRow = values => {
+    tableData.push(values)
+    setTableData([...tableData])
+  }
+
+  const handleSaveRowEdits = async ({ exitEditingMode, row, values }) => {
+    if (!Object.keys(validationErrors).length) {
+      tableData[row.index] = values
+
+      //send/receive api updates here, then refetch or update local table data for re-render
+      setTableData([...tableData])
+      exitEditingMode() //required to exit editing mode and close modal
+    }
+  }
+
+  const handleCancelRowEdits = () => {
+    setValidationErrors({})
+  }
+
+  const handleDeleteRow = useCallback(
+    row => {
+      if (!confirm(`Are you sure you want to delete ${row.getValue('firstName')}`)) {
+        return
+      }
+
+      //send api delete request here, then refetch or update local table data for re-render
+      tableData.splice(row.index, 1)
+      setTableData([...tableData])
+    },
+    [tableData]
+  )
+
+  const getCommonEditTextFieldProps = useCallback(
+    cell => {
+      return {
+        error: !!validationErrors[cell.id],
+        helperText: validationErrors[cell.id],
+        onBlur: event => {
+          const isValid =
+            cell.column.id === 'email'
+              ? validateEmail(event.target.value)
+              : cell.column.id === 'age'
+              ? validateAge(+event.target.value)
+              : validateRequired(event.target.value)
+          if (!isValid) {
+            //set validation error for cell if invalid
+            setValidationErrors({
+              ...validationErrors,
+              [cell.id]: `${cell.column.columnDef.header} is required`
+            })
+          } else {
+            //remove validation error for cell if valid
+            delete validationErrors[cell.id]
+            setValidationErrors({
+              ...validationErrors
+            })
+          }
+        }
+      }
+    },
+    [validationErrors]
+  )
+
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: 'id',
+        header: 'ID',
+        enableColumnOrdering: false,
+        enableEditing: false, //disable editing on this column
+        enableSorting: false,
+        size: 80
+      },
+      {
+        accessorKey: 'firstName',
+        header: 'First Name',
+        size: 140,
+        muiTableBodyCellEditTextFieldProps: ({ cell }) => ({
+          ...getCommonEditTextFieldProps(cell)
+        })
+      },
+      {
+        accessorKey: 'lastName',
+        header: 'Last Name',
+        size: 140,
+        muiTableBodyCellEditTextFieldProps: ({ cell }) => ({
+          ...getCommonEditTextFieldProps(cell)
+        })
+      },
+      {
+        accessorKey: 'email',
+        header: 'Email',
+        muiTableBodyCellEditTextFieldProps: ({ cell }) => ({
+          ...getCommonEditTextFieldProps(cell),
+          type: 'email'
+        })
+      },
+      {
+        accessorKey: 'age',
+        header: 'Age',
+        size: 80,
+        muiTableBodyCellEditTextFieldProps: ({ cell }) => ({
+          ...getCommonEditTextFieldProps(cell),
+          type: 'number'
+        })
+      },
+      {
+        accessorKey: 'state',
+        header: 'State',
+        muiTableBodyCellEditTextFieldProps: {
+          select: true, //change to select for a dropdown
+          children: states.map(state => (
+            <MenuItem key={state} value={state}>
+              {state}
+            </MenuItem>
+          ))
+        }
+      }
+    ],
+    [getCommonEditTextFieldProps]
+  )
+
+  // ** State
+  const [role, setRole] = useState('')
+  const [plan, setPlan] = useState('')
+  const [value, setValue] = useState('')
+  const [status, setStatus] = useState('')
+  const [pageSize, setPageSize] = useState(10)
+  const [addUserOpen, setAddUserOpen] = useState(false)
+
+  // ** Hooks
+  // const dispatch = useDispatch()
+  // const store = useSelector(state => state.user)
+  // useEffect(() => {
+  //   dispatch(
+  //     fetchData({
+  //       role,
+  //       status,
+  //       q: value,
+  //       currentPlan: plan
+  //     })
+  //   )
+  // }, [dispatch, plan, role, status, value])
+
+  const handleFilter = useCallback(val => {
+    setValue(val)
+  }, [])
+
+  const handleRoleChange = useCallback(e => {
+    setRole(e.target.value)
+  }, [])
+
+  const handlePlanChange = useCallback(e => {
+    setPlan(e.target.value)
+  }, [])
+
+  const handleStatusChange = useCallback(e => {
+    setStatus(e.target.value)
+  }, [])
+  const toggleAddUserDrawer = () => setAddUserOpen(!addUserOpen)
+
   return (
     <Grid container spacing={6}>
       <Grid item xs={12}>
+        {/* <HorizontalLayout /> */}
         <Card>
-          <CardHeader title='Kick start your project 🚀'></CardHeader>
+          <CardHeader title='Search Filters' sx={{ pb: 4, '& .MuiCardHeader-title': { letterSpacing: '.15px' } }} />
           <CardContent>
-            <Typography sx={{ mb: 2 }}>All the best for your new project.</Typography>
-            <Typography>
-              Please make sure to read our Template Documentation to understand where to go from here and how to use our
-              template.
-            </Typography>
+            <Grid container spacing={6}>
+              <Grid item sm={4} xs={12}>
+                <FormControl fullWidth>
+                  <InputLabel id='role-select'>Select Role</InputLabel>
+                  <Select
+                    fullWidth
+                    value={role}
+                    id='select-role'
+                    label='Select Role'
+                    labelId='role-select'
+                    onChange={handleRoleChange}
+                    inputProps={{ placeholder: 'Select Role' }}
+                  >
+                    <MenuItem value=''>Select Role</MenuItem>
+                    <MenuItem value='admin'>Admin</MenuItem>
+                    <MenuItem value='author'>Author</MenuItem>
+                    <MenuItem value='editor'>Editor</MenuItem>
+                    <MenuItem value='maintainer'>Maintainer</MenuItem>
+                    <MenuItem value='subscriber'>Subscriber</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item sm={4} xs={12}>
+                <FormControl fullWidth>
+                  <InputLabel id='plan-select'>Select Plan</InputLabel>
+                  <Select
+                    fullWidth
+                    value={plan}
+                    id='select-plan'
+                    label='Select Plan'
+                    labelId='plan-select'
+                    onChange={handlePlanChange}
+                    inputProps={{ placeholder: 'Select Plan' }}
+                  >
+                    <MenuItem value=''>Select Plan</MenuItem>
+                    <MenuItem value='basic'>Basic</MenuItem>
+                    <MenuItem value='company'>Company</MenuItem>
+                    <MenuItem value='enterprise'>Enterprise</MenuItem>
+                    <MenuItem value='team'>Team</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item sm={4} xs={12}>
+                <FormControl fullWidth>
+                  <InputLabel id='status-select'>Select Status</InputLabel>
+                  <Select
+                    fullWidth
+                    value={status}
+                    id='select-status'
+                    label='Select Status'
+                    labelId='status-select'
+                    onChange={handleStatusChange}
+                    inputProps={{ placeholder: 'Select Role' }}
+                  >
+                    <MenuItem value=''>Select Role</MenuItem>
+                    <MenuItem value='pending'>Pending</MenuItem>
+                    <MenuItem value='active'>Active</MenuItem>
+                    <MenuItem value='inactive'>Inactive</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+            </Grid>
           </CardContent>
-        </Card>
-      </Grid>
-      <Grid item xs={12}>
-        <Card>
-          <CardHeader title='ACL and JWT 🔒'></CardHeader>
-          <CardContent>
-            <Typography sx={{ mb: 2 }}>
-              Access Control (ACL) and Authentication (JWT) are the two main security features of our template and are implemented in the starter-kit as well.
-            </Typography>
-            <Typography>Please read our Authentication and ACL Documentations to get more out of them.</Typography>
-          </CardContent>
+          <Divider />
+          {/* <TableHeader value={value} handleFilter={handleFilter} toggle={toggleAddUserDrawer} /> */}
+          <MaterialReactTable
+            displayColumnDefOptions={{
+              'mrt-row-actions': {
+                muiTableHeadCellProps: {
+                  align: 'center'
+                },
+                size: 120
+              }
+            }}
+            columns={columns}
+            data={tableData}
+            editingMode='modal' //default
+            enableColumnOrdering
+            enableEditing
+            enableGrouping
+            onEditingRowSave={handleSaveRowEdits}
+            onEditingRowCancel={handleCancelRowEdits}
+            renderRowActions={({ row, table }) => (
+              <Box sx={{ display: 'flex', gap: '1rem' }}>
+                <Tooltip arrow placement='left' title='Edit'>
+                  <IconButton onClick={() => table.setEditingRow(row)}>
+                    <Edit />
+                  </IconButton>
+                </Tooltip>
+                <Tooltip arrow placement='right' title='Delete'>
+                  <IconButton color='error' onClick={() => handleDeleteRow(row)}>
+                    <Delete />
+                  </IconButton>
+                </Tooltip>
+              </Box>
+            )}
+            renderTopToolbarCustomActions={() => (
+              <Button color='secondary' onClick={() => setCreateModalOpen(true)} variant='contained'>
+                Create New Account
+              </Button>
+            )}
+          />
+          <CreateNewAccountModal
+            columns={columns}
+            open={createModalOpen}
+            onClose={() => setCreateModalOpen(false)}
+            onSubmit={handleCreateNewRow}
+          />
         </Card>
       </Grid>
     </Grid>
   )
 }
+
+//example of creating a mui dialog modal for creating new rows
+export const CreateNewAccountModal = ({ open, columns, onClose, onSubmit }) => {
+  const [values, setValues] = useState(() =>
+    columns.reduce((acc, column) => {
+      acc[column.accessorKey ?? ''] = ''
+
+      return acc
+    }, {})
+  )
+
+  const handleSubmit = () => {
+    //put your validation logic here
+    onSubmit(values)
+    onClose()
+  }
+
+  return (
+    <Dialog open={open}>
+      <DialogTitle textAlign='center'>Create New Account</DialogTitle>
+      <DialogContent>
+        <form onSubmit={e => e.preventDefault()}>
+          <Stack
+            sx={{
+              width: '100%',
+              minWidth: { xs: '300px', sm: '360px', md: '400px' },
+              gap: '1.5rem'
+            }}
+          >
+            {columns.map(column => (
+              <TextField
+                key={column.accessorKey}
+                label={column.header}
+                name={column.accessorKey}
+                onChange={e => setValues({ ...values, [e.target.name]: e.target.value })}
+              />
+            ))}
+          </Stack>
+        </form>
+      </DialogContent>
+      <DialogActions sx={{ p: '1.25rem' }}>
+        <Button onClick={onClose}>Cancel</Button>
+        <Button color='secondary' onClick={handleSubmit} variant='contained'>
+          Create New Account
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
+
+const validateRequired = value => !!value.length
+
+const validateEmail = email =>
+  !!email.length &&
+  email
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )
+const validateAge = age => age >= 18 && age <= 50
 
 export default Home
