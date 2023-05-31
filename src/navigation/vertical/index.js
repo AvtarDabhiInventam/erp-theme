974bf6d9451940132ac7e960fee994ad06093a50
dhavalinventam <dhaval@inventam.com>
2023-05-31 11:04:13 +0530

    First commit

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -1,21 +1,56 @@
 const navigation = () => {
   return [
+    // {
+    //   title: 'Masters',
+    //   path: '/home',
+    //   icon: 'mdi:home-outline',
+    // },
     {
-      title: 'Home',
-      path: '/home',
-      icon: 'mdi:home-outline',
+      title: 'Masters',
+      icon: 'mdi:file-document-outline',
+      children: [
+        {
+          title: 'Account Master',
+          path: '/Masters/account-master'
+        },
+        {
+          title: 'Item Master',
+          path: ''
+        },
+        {
+          title: 'Quality Master',
+          path: ''
+        },
+        {
+          title: 'SubHead Master',
+          path: ''
+        },
+        {
+          title: 'Company Master',
+          path: ''
+        },
+        {
+          title: 'City Master',
+          path: ''
+        },
+        {
+          title: 'Currency Master',
+          path: ''
+        },
+        {
+          title: 'Sub Company Master',
+          path: ''
+        },
+        {
+          title: 'Master Data',
+          path: ''
+        }
+      ]
     },
     {
-      title: 'Second Page',
+      title: 'Transaction',
       path: '/second-page',
       icon: 'mdi:email-outline',
-    },
-    {
-      path: '/acl',
-      action: 'read',
-      subject: 'acl-page',
-      title: 'Access Control',
-      icon: 'mdi:shield-outline',
     }
   ]
 }
