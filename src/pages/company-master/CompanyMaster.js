ec3631cfe042891d36b339f6a5eac53188ce6194
AvtarDabhiInventam <108725375+AvtarDabhiInventam@users.noreply.github.com>
2023-06-02 16:42:17 +0530

    company master

---
+++ src/pages/company-master/CompanyMaster.js
@@ -0,0 +1,323 @@
+// ** MUI Imports
+import { forwardRef, useState } from 'react'
+import Card from '@mui/material/Card'
+import Grid from '@mui/material/Grid'
+import Typography from '@mui/material/Typography'
+import CardHeader from '@mui/material/CardHeader'
+import CardContent from '@mui/material/CardContent'
+import {
+  Button,
+  CardActions,
+  Checkbox,
+  Divider,
+  FormControl,
+  FormControlLabel,
+  Icon,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  MenuItem,
+  OutlinedInput,
+  Select,
+  Switch,
+  Tab,
+  TextField
+} from '@mui/material'
+import { DatePicker, TabContext, TabList, TabPanel } from '@mui/lab'
+import { Box, border } from '@mui/system'
+
+const CustomInput = forwardRef((props, ref) => {
+  return <TextField fullWidth {...props} inputRef={ref} label='Birth Date' autoComplete='off' />
+})
+
+const CompanyMaster = () => {
+  // ** States
+  const [date, setDate] = useState(null)
+  const [language, setLanguage] = useState([])
+
+  const [values, setValues] = useState({
+    password: '',
+    password2: '',
+    showPassword: false,
+    showPassword2: false
+  })
+
+  // Handle Password
+  const handlePasswordChange = prop => event => {
+    setValues({ ...values, [prop]: event.target.value })
+  }
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword })
+  }
+
+  const handleMouseDownPassword = event => {
+    event.preventDefault()
+  }
+
+  // Handle Confirm Password
+  const handleConfirmChange = prop => event => {
+    setValues({ ...values, [prop]: event.target.value })
+  }
+
+  const handleClickShowConfirmPassword = () => {
+    setValues({ ...values, showPassword2: !values.showPassword2 })
+  }
+
+  const handleMouseDownConfirmPassword = event => {
+    event.preventDefault()
+  }
+
+  // Handle Select
+  const handleSelectChange = event => {
+    setLanguage(event.target.value)
+  }
+
+  return (
+    <Card>
+      <CardHeader title='Company Master Details' />
+      <Divider sx={{ m: '0 !important' }} />
+      <form onSubmit={e => e.preventDefault()}>
+        <CardContent>
+          <Grid container spacing={5}>
+            <Grid item xs={12}>
+              <Typography variant='body2' sx={{ fontWeight: 600 }}>
+                1. Company Details
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <FormControl fullWidth>
+                <TextField fullWidth label='Company Name' placeholder='Leonard' />
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <FormControl fullWidth>
+                <InputLabel id='form-layouts-separator-select-label'>GST No</InputLabel>
+                <Select
+                  label='Country'
+                  defaultValue=''
+                  id='form-layouts-separator-select'
+                  labelId='form-layouts-separator-select-label'
+                >
+                  <MenuItem value='UK'>UK</MenuItem>
+                  <MenuItem value='USA'>USA</MenuItem>
+                  <MenuItem value='Australia'>Australia</MenuItem>
+                  <MenuItem value='Germany'>Germany</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <FormControl fullWidth>
+                <InputLabel id='form-layouts-separator-select-label'>Type of Dealer</InputLabel>
+                <Select
+                  label='Country'
+                  defaultValue=''
+                  id='form-layouts-separator-select'
+                  labelId='form-layouts-separator-select-label'
+                >
+                  <MenuItem value='UK'>UK</MenuItem>
+                  <MenuItem value='USA'>USA</MenuItem>
+                  <MenuItem value='Australia'>Australia</MenuItem>
+                  <MenuItem value='Germany'>Germany</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={12}>
+              <TextField multiline label='Address' id='textarea-filled-controlled' rows={3} fullWidth />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <FormControl fullWidth>
+                <InputLabel id='form-layouts-separator-select-label'>Country</InputLabel>
+                <Select
+                  label='Country'
+                  defaultValue=''
+                  id='form-layouts-separator-select'
+                  labelId='form-layouts-separator-select-label'
+                >
+                  <MenuItem value='UK'>UK</MenuItem>
+                  <MenuItem value='USA'>USA</MenuItem>
+                  <MenuItem value='Australia'>Australia</MenuItem>
+                  <MenuItem value='Germany'>Germany</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <FormControl fullWidth>
+                <InputLabel id='form-layouts-separator-select-label'>State/Origin</InputLabel>
+                <Select
+                  label='Country'
+                  defaultValue=''
+                  id='form-layouts-separator-select'
+                  labelId='form-layouts-separator-select-label'
+                >
+                  <MenuItem value='UK'>UK</MenuItem>
+                  <MenuItem value='USA'>USA</MenuItem>
+                  <MenuItem value='Australia'>Australia</MenuItem>
+                  <MenuItem value='Germany'>Germany</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <FormControl fullWidth>
+                <InputLabel id='form-layouts-separator-select-label'>City</InputLabel>
+                <Select
+                  label='Country'
+                  defaultValue=''
+                  id='form-layouts-separator-select'
+                  labelId='form-layouts-separator-select-label'
+                >
+                  <MenuItem value='UK'>UK</MenuItem>
+                  <MenuItem value='USA'>USA</MenuItem>
+                  <MenuItem value='Australia'>Australia</MenuItem>
+                  <MenuItem value='Germany'>Germany</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <FormControl fullWidth>
+                <TextField fullWidth label='Pin Code' placeholder='Leonard' />
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={2}>
+              <FormControl fullWidth>
+                <TextField fullWidth label='Sez' placeholder='Sez' />
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={2}>
+              <FormControl fullWidth>
+                <TextField fullWidth label='Company Name' placeholder='Leonard' />
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <FormControl fullWidth>
+                <InputLabel id='form-layouts-separator-select-label'>Accounting Year</InputLabel>
+                <Select
+                  label='Country'
+                  defaultValue=''
+                  id='form-layouts-separator-select'
+                  labelId='form-layouts-separator-select-label'
+                >
+                  <MenuItem value='UK'>UK</MenuItem>
+                  <MenuItem value='USA'>USA</MenuItem>
+                  <MenuItem value='Australia'>Australia</MenuItem>
+                  <MenuItem value='Germany'>Germany</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <FormControl fullWidth>
+                <TextField fullWidth label='Comapany Type' placeholder='Comapany Type' />
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <FormControl fullWidth>
+                <TextField fullWidth label='Phone' placeholder='Phone' />
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <FormControl fullWidth>
+                <TextField fullWidth label='Mobile' placeholder='Mobile' />
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <FormControl fullWidth>
+                <TextField fullWidth label='Whatsapp No' placeholder='Whatsapp No' />
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <FormControl fullWidth>
+                <TextField fullWidth label='Telegram No' placeholder='Telegram No' />
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <FormControl fullWidth>
+                <TextField fullWidth label='Email' placeholder='Email' />
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <FormControl fullWidth>
+                <TextField fullWidth label='Password' placeholder='Password' />
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <Divider sx={{ mb: '0 !important' }} />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant='body2' sx={{ fontWeight: 600 }}>
+                2. Personal Info
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField fullWidth label='First Name' placeholder='Leonard' />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField fullWidth label='Last Name' placeholder='Carter' />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <FormControl fullWidth>
+                <InputLabel id='form-layouts-separator-select-label'>Country</InputLabel>
+                <Select
+                  label='Country'
+                  defaultValue=''
+                  id='form-layouts-separator-select'
+                  labelId='form-layouts-separator-select-label'
+                >
+                  <MenuItem value='UK'>UK</MenuItem>
+                  <MenuItem value='USA'>USA</MenuItem>
+                  <MenuItem value='Australia'>Australia</MenuItem>
+                  <MenuItem value='Germany'>Germany</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <FormControl fullWidth>
+                <InputLabel id='form-layouts-separator-multiple-select-label'>Language</InputLabel>
+                <Select
+                  multiple
+                  value={language}
+                  onChange={handleSelectChange}
+                  id='form-layouts-separator-multiple-select'
+                  labelId='form-layouts-separator-multiple-select-label'
+                  input={<OutlinedInput label='Language' id='select-multiple-language' />}
+                >
+                  <MenuItem value='English'>English</MenuItem>
+                  <MenuItem value='French'>French</MenuItem>
+                  <MenuItem value='Spanish'>Spanish</MenuItem>
+                  <MenuItem value='Portuguese'>Portuguese</MenuItem>
+                  <MenuItem value='Italian'>Italian</MenuItem>
+                  <MenuItem value='German'>German</MenuItem>
+                  <MenuItem value='Arabic'>Arabic</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <DatePicker
+                selected={date}
+                showYearDropdown
+                showMonthDropdown
+                placeholderText='MM-DD-YYYY'
+                customInput={<CustomInput />}
+                id='form-layouts-separator-date'
+                onChange={date => setDate(date)}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField fullWidth label='Phone No.' placeholder='+1-123-456-8790' />
+            </Grid>
+          </Grid>
+        </CardContent>
+        <Divider sx={{ m: '0 !important' }} />
+        <CardActions>
+          <Button size='large' type='submit' sx={{ mr: 2 }} variant='contained'>
+            Submit
+          </Button>
+          <Button type='reset' size='large' color='secondary' variant='outlined'>
+            Reset
+          </Button>
+        </CardActions>
+      </form>
+    </Card>
+  )
+}
+
+export default CompanyMaster
